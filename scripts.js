2f59cbe116c0a2b91cd493df2fd039bd9409d47c
TY Program (Richard) <typrogram.overstock@gmail.com>
2023-03-13 17:29:05 +0000

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function getMoonPhases () {
+  // We get today's date by doing new Date()
+  var todaysDate = new Date();
+
+  // We then extract the year and month, so that we can add those variables to the url
+  var year = todaysDate.getFullYear();
+  var month = todaysDate.getMonth();
+
+  fetch('https://www.icalendar37.net/lunar/api/?year=' + year + '&month=' + (month + 1))
+    .then(response => response.json())
+    .then(response => {
+        // This is where you populate the html elements and element attributes with values from the result
+    })
+    .catch(err => console.error(err));
+}
